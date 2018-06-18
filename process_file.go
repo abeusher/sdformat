aba2885f9252d95c459f4477b8129ee35a9a0b64
Abe Usher <abe.usher@gmail.com>
2018-06-18 13:45:11 -0400

    minor updates to make variable names consistent

--- process_file.go
+++ process_file.go
@@ -3,13 +3,91 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"github.com/abeusher/selectphone_processing/util"
-	"github.com/sirupsen/logrus"
 	"math/rand"
 	"os"
 	"strings"
 	"time"
+
+	"github.com/abeusher/sdformat/util"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
+
+/*
+
+This is fun :)
+
+TODO:
+Create SdFormat struct
+Create method to make a 'new' SdFormat struct
+Create method to populate SdFormat struct
+Create method to compute geohash9 with given SdFormat object
+
+*/
+
+/*
+headers = 'sd_unique_id,title,first_name,initial,last_name,
+address1,address2,city,state,zipcode,zipcode_4,county_name,geo_level,
+latitude,longitude,geohash8,geohash5,msa,cbsa,fips_state,fips_county,census_tract,
+census_block_group,census_block,full_census_block_id,first_in_household,
+child_present,age,home_phone,estimated_income,length_of_residence,dwelling_type,
+homeowner_type,gender,marital_status,estimated_wealth,estimated_home_value,
+cellphone,email1,email2,education,business_owner_status,conservative_political_donor,
+liberal_political_donor,veterans_donor,do_not_call_list,timezone,birth_year,date_updated'.upper().replace(',','\t')
+*/
+
+//SdFormat is the struct for handing input data
+type SdFormat struct {
+	uniqueID                   string
+	title                      string
+	firstName                  string
+	initial                    string
+	lastName                   string
+	address1                   string
+	address2                   string
+	city                       string
+	state                      string
+	zipcode                    string
+	zipcode4                   string
+	countyName                 string
+	geoLevel                   string
+	latitude                   string
+	longitude                  string
+	geohash9                   string
+	geohash5                   string
+	msa                        string
+	cbsa                       string
+	fipsState                  string
+	fipsCounty                 string
+	censusTract                string
+	censusBlockGroup           string
+	censusBlock                string
+	fullCensusBlockID          string
+	firstInHousehold           string
+	childPresent               string
+	age                        string
+	homePhone                  string
+	estimatedIncome            string
+	lengthOfResidence          string
+	dwellingType               string
+	homeownerType              string
+	gender                     string
+	maritalStatus              string
+	estimatedSealth            string
+	estimatedHomeValue         string
+	cellphone                  string
+	email1                     string
+	email2                     string
+	education                  string
+	businessOwnerStatus        string
+	conservativePoliticalDonor string
+	liberalPoliticalDonor      string
+	veteransDonor              string
+	doNotCallList              string
+	timezone                   string
+	birthYear                  string
+	dateUpdated                string
+}
 
 //global variables
 var (
@@ -51,17 +129,23 @@ func processNameAddress(parts []string) {
 	baseInt := 1000000000
 	randomInt := rand.Intn(500000)
 	totalInt := baseInt + randomInt
-	uniqueID := string(totalInt)
-	firstName := parts[2]
-	middleName := parts[3]
-	lastName := parts[4]
-	namePrefix := parts[6]
-	address1 := parts[14]
-	address2 := parts[15]
-	apartment := parts[15]
+	/*
+		uniqueID := string(totalInt)
+		firstName := parts[2]
+		middleName := parts[3]
+		lastName := parts[4]
+		namePrefix := parts[6]
+		address1 := parts[14]
+		address2 := parts[15]
+		apartment := parts[15]
 
-	var data []string
-	data = append(data, []string{firstName, lastName, namePrefix}...)
+		var data []string
+		data = append(data, []string{firstName, lastName, namePrefix}...)
+	*/
+	if false {
+		fmt.Println(nameAddressItems)
+		fmt.Println(totalInt)
+	}
 
 }
 
